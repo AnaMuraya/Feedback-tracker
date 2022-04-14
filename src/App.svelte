7722f0e112845053aa672b7a07d3f77175690264
Ana Muraya <anamuraya9@gmail.com>
2022-04-14 09:55:53 +0300

    Added the feedback stats

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,34 @@
 <script>
 	import FeedbackList from "./components/FeedbackList.svelte"
+	import FeedbackStats from './components/FeedbackStats.svelte'
 	// FeedbackList data
 	let feedbackList = [
 		{
 			id: '1',
-			rating: '10',
+			rating: 10,
 			message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aliquam iure voluptatibus distinctio aperiam minus, itaque, reprehenderit quidem autem corrupti ab deserunt nulla, adipisci pariatur quod laudantium quasi possimus est!'
 		},
 		{
 			id: '2',
-			rating: '8',
+			rating: 8,
 			message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aliquam iure voluptatibus distinctio aperiam minus, itaque, reprehenderit quidem autem corrupti ab deserunt nulla, adipisci pariatur quod laudantium quasi possimus est!'
 		},
 		{
 			id: '3',
-			rating: '9',
+			rating: 9,
 			message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aliquam iure voluptatibus distinctio aperiam minus, itaque, reprehenderit quidem autem corrupti ab deserunt nulla, adipisci pariatur quod laudantium quasi possimus est!'
 		},
 		{
 			id: '4',
-			rating: '7',
+			rating: 7,
 			message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aliquam iure voluptatibus distinctio aperiam minus, itaque, reprehenderit quidem autem corrupti ab deserunt nulla, adipisci pariatur quod laudantium quasi possimus est!'
 		}
 	]
-		// delete feedback dispatcher function
+
+	$: count = feedbackList.length
+	$: average = feedbackList.reduce((a, {rating}) => a + rating , 0) / count
+		
+	// delete feedback dispatcher function
 	const deleteItem = (e) => {
 		const ItemID = e.detail
 		feedbackList = feedbackList.filter((item) => item.id != ItemID)
@@ -31,5 +36,6 @@
 
 </script>
 <main class="container">
+	<FeedbackStats {count} {average}/>
 	<FeedbackList {feedbackList} on:deleteItem={deleteItem}/>
 </main>
---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let count
+    export let average
+</script>
+
+<div class="feedbackStats">
+    <h4>{count} Reviews</h4>
+    <h4>Ratings: {average}</h4>
+</div>
+<style>
+    .feedbackStats{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
